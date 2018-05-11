d1814b013fe77e24f9f3923767ed02fb79bf94e6
yjwu <s1041535@gm.pu.edu.tw>
2018-05-11 13:53:57 +0800

    init again

---
+++ app/src/main/java/com/yr/bustop/HomeRouteActivity.java
@@ -0,0 +1,13 @@
+package com.yr.bustop;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HomeRouteActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_route);
+    }
+}
---
+++ app/src/main/java/com/yr/bustop/RealTimeBusActivity.java
@@ -0,0 +1,13 @@
+package com.yr.bustop;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RealTimeBusActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_real_time_bus);
+    }
+}
