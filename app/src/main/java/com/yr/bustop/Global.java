eee22cd77082647b9a9797192d7ee875d9c61e25
Steve <Steve@gmail.com>
2018-05-24 20:48:19 +0800

    接API(Failed)

---
+++ app/src/main/java/com/yr/bustop/Global.java
@@ -0,0 +1,10 @@
+package com.yr.bustop;
+
+public class Global {
+    static String EstimatedTimeOfArrival_URL = "http://ptx.transportdata.tw/MOTC/v2/Bus/EstimatedTimeOfArrival/City/Taichung/55?$format=JSON";
+//    static String EstimatedTimeOfArrival_URL = "http://140.128.10.150:8080/busTop/";
+    static String TAG_StopName = "StopName";
+    static String TAG_RouteID = "RouteID";
+    static String TAG_Direction = "Direction";
+    static String TAG_EstimateTime = "EstimateTime";
+}
